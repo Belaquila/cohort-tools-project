d1aae1cf33e36d23e0a47f6ca656b4fce26f317c
Ahmed Belabdia <ab.belabdia@gmail.com>
2024-11-11 17:23:50 +0100

    add server config

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
+const cohorts = require("./cohorts.json")
+const students = require("./students.json")
+const cors = require("cors");
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -10,6 +13,9 @@ const PORT = 5005;
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
+app.use(cors({
+  origin: ['http://localhost:5170'],
+}))
 
 
 // MIDDLEWARE
@@ -28,6 +34,14 @@ app.use(cookieParser());
 app.get("/docs", (req, res) => {
   res.sendFile(__dirname + "/views/docs.html");
 });
+
+app.get("/api/cohorts", (req,res)=>{
+  res.json(cohorts)
+})
+
+app.get("/api/students", (req,res)=>{
+  res.json(students)
+})
 
 
 // START SERVER
